28709018ec0942db723ccd74b5d4af6ce3eb7e79
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-02-23 10:44:31 +0600

    users api

---
+++ app.js
@@ -0,0 +1,17 @@
+// user 
+
+function loadUser(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUser(data))
+}
+function displayUser(data){
+    const ul = document.getElementById('users-list');
+    for(const user of data){
+        // console.log(user.name);
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li);
+    }
+    
+}
